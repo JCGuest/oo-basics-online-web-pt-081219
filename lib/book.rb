408376c210803c854b2179e54a8cd309275449f8
John Guest <jcguest90@gmail.com>
2019-08-30 16:51:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,45 +1,16 @@
+# book.rb
+ 
 class Book
-  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
   def initialize(title)
     @title = title
   end
-  
-  def title
-    @title
-  end
-  
-  
-  def author=(author)
-    @author = author
-  end
-  
-  
-  def author
-    @author
-  end
-  
-  
-  def page_count=(num)
-    @page_count = num
-  end
  
-  def page_count
-    @page_count
-  end 
-  
-  
-  def genre=(book_genre)
-    @genre = book_genre
-  end
-  
-  
-  def genre 
-  @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
-end 
+ 
+end
 
